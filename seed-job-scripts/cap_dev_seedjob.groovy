f5b86541dfa8f929d1b845cf17153b4bd1069ce8
Collins Orighose <collinsefe@yahoo.com>
2024-11-02 22:57:53 +0000

    update Jenkisnfile

--- seed-job-scripts/cap_dev_seedjob.groovy
+++ seed-job-scripts/cap_dev_seedjob.groovy
@@ -26,8 +26,40 @@ job("CAP_${env.toUpperCase()}-Job"){
     disabled(false)
     concurrentBuild(false)
 
-    steps{
-        shell(readFileFromWorkspace("resources/${env.toUpperCase()}/run_dev.sh"))
+ String env = 'dev'
+
+def githubUrl = 'https://github.com'
+def gitCreds = "prodigital-collinsefe"
+
+job("CAP_${env.toUpperCase()}-Job") {
+    description("This Job is used to create the Node Server and its versioned. Changes should be made through the repo")
+    keepDependencies(false)
+
+    multiscm {
+        git {
+            remote {
+                name('origin')
+                url ("https://gitlab.com/cloud-devops-assignments/spring-boot-react-example")
+                credentials(gitCreds)
+            }
+            extensions {
+                branch('*/main')
+            }
+        }
+    }
+
+    disabled(false)
+    concurrentBuild(false)
+
+    steps {
+        // Use the Jenkinsfile in the resources folder
+        // Ensure to use the correct path where the Jenkinsfile is located
+        pipeline {
+            script {
+                // Point to the Jenkinsfile for the pipeline definition
+                load "resources/${env.toUpperCase()}/Jenkinsfile"
+            }
+        }
     }
 
     publishers{

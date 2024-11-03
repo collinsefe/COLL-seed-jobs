8c67ee79cf76a06a631c5dfced962e3745ed2253
Collins Orighose <collinsefe@yahoo.com>
2024-11-03 00:09:03 +0000

    update Jenkisnfile

--- resources/DEV/run_pipeline.sh
+++ resources/DEV/run_pipeline.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Navigate to the project directory (adjust the path as necessary)
-
 git clone https://gitlab.com/cloud-devops-assignments/spring-boot-react-example.git
 
-cd spring-boot-react-example
+cd spring-boot-react-example || exit
 
 ls -la 
 sleep 10
-
 
 # Build the Maven application
 echo "Building the application..."
@@ -20,27 +18,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Run the Spring Boot application
+# Run the Spring Boot application in detached mode
 echo "Running the Spring Boot application..."
-java -jar target/*.jar --server.port=8081 &
-# java -jar target/*.jar &
+nohup java -jar target/*.jar --server.port=8081 > application.log 2>&1 &
 
-# Store the PID of the Java process
-APP_PID=$!
+# Print the PID of the detached process
+echo "Application started in detached mode with PID: $!"
 
-# Give the application a moment to start
+# Optionally, you can give the application a moment to start
 sleep 10
 
 # Test the API (you may adjust the curl command as necessary)
 echo "Testing the API..."
-curl -v -u greg:turnquist http://localhost:8080/api/employees/3
-
-# Optionally, you could wait for the user to exit the application
-# echo "Press [CTRL+C] to stop the application."
-# wait $APP_PID
-
-# If you want to stop the application on exit
-trap "kill $APP_PID" EXIT
-
-# Keep the script running while the application is running
-wait $APP_PID
+curl -v -u greg:turnquist http://localhost:8081/api/employees/3

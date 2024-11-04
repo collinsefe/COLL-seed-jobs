7850cba1392b0f63cbe491ae090d4722a3a84049
Collins Orighose <collinsefe@yahoo.com>
2024-11-04 11:59:44 +0000

    update envs

--- resources/DEV/run_backend.sh
+++ resources/DEV/run_backend.sh
@@ -15,7 +15,7 @@ mvn clean install
 
 # Run the Spring Boot application in detached mode
 echo "Running the Spring Boot application..."
-nohup java -jar target/*.jar --server.port=${8081} > application.log 2>&1 &
+nohup java -jar target/*.jar --server.port=${port} > application.log 2>&1 &
 
 # Print the PID of the detached process
 echo "Application started in detached mode with PID: $!"
@@ -25,7 +25,7 @@ sleep 10
 
 # Test the API (you may adjust the curl command as necessary)
 echo "Testing the API..."
-curl -v -u greg:turnquist http://localhost:${8081}/api/employees/3
+curl -v -u greg:turnquist http://localhost:${port}/api/employees/3
 
 while true; do 
     sleep 60

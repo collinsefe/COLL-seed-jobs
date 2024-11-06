735dc73acf6041a9cde668d4a818a9576c9b4f69
Collins Orighose <collinsefe@yahoo.com>
2024-11-06 12:52:49 +0000

    lint files

--- seed-job-scripts/cap_backend_seedjob.groovy
+++ seed-job-scripts/cap_backend_seedjob.groovy
@@ -1,42 +1,41 @@
 def githubUrl = 'https://github.com/collinsefe/spring-boot-react-example.git'
-def gitCreds = "prodigital-collinsefe"
-
+def gitCreds = 'prodigital-collinsefe'
 
 def environments = [
     demo: [
-        name: "Demo",
-        branch: "demo"
+        name: 'Demo',
+        branch: 'demo'
     ],
     test: [
-        name: "Testing",
-        branch: "test"
+        name: 'Testing',
+        branch: 'test'
     ],
 ]
 
 environments.each { env, config ->
-pipelineJob("COLL-BACKEND-${env.toUpperCase()}-Job") {
-    description("This Job is used to create the Node Server and is versioned. Changes should be made through the repo.")
-    keepDependencies(false)
+    pipelineJob("COLL-BACKEND-${env.toUpperCase()}-Job") {
+        description('This Job is used to create the Node Server and is versioned. Changes should be made through the repo.')
+        keepDependencies(false)
 
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url(githubUrl)
-                        credentials(gitCreds)
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(githubUrl)
+                            credentials(gitCreds)
+                        }
+                        branch("*/${config.branch}")
                     }
-                    branch("*/${config.branch}")
                 }
             }
         }
-    }
-    triggers {
-            githubPush()  
+        triggers {
+            githubPush()
         }
 
-    properties {
-        disableConcurrentBuilds()
+        properties {
+            disableConcurrentBuilds()
+        }
     }
- }
 }
--- seed-job-scripts/cap_frontend_dev_seedjob.groovy
+++ seed-job-scripts/cap_frontend_dev_seedjob.groovy
@@ -1,42 +1,41 @@
 def githubUrl = 'https://github.com/collinsefe/dotnet-app-image.git'
-def gitCreds = "prodigital-collinsefe"
-
+def gitCreds = 'prodigital-collinsefe'
 
 def environments = [
     dev: [
-        name: "Development",
-        branch: "dev"
+        name: 'Development',
+        branch: 'dev'
     ],
     test: [
-        name: "Testing",
-        branch: "test"
+        name: 'Testing',
+        branch: 'test'
     ],
 ]
 
 environments.each { env, config ->
-pipelineJob("COLL-FRONTEND-${env.toUpperCase()}-Job") {
-    description("This Job is used to create the Node Server and is versioned. Changes should be made through the repo.")
-    keepDependencies(false)
+    pipelineJob("COLL-FRONTEND-${env.toUpperCase()}-Job") {
+        description('This Job is used to create the Node Server and is versioned. Changes should be made through the repo.')
+        keepDependencies(false)
 
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url(githubUrl)
-                        credentials(gitCreds)
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(githubUrl)
+                            credentials(gitCreds)
+                        }
+                        branch("*/${config.branch}")
                     }
-                    branch("*/${config.branch}")
                 }
             }
         }
-    }
-    triggers {
-            githubPush()  
+        triggers {
+            githubPush()
         }
 
-    properties {
-        disableConcurrentBuilds()
+        properties {
+            disableConcurrentBuilds()
+        }
     }
- }
 }

10ebc608c985db1f867b4b0ed599a73656752b6f
Collins Orighose <collinsefe@yahoo.com>
2024-11-14 18:40:35 +0000

    change job names

---
+++ seed-job-scripts/app_backend_seedjob.groovy
@@ -0,0 +1,41 @@
+def githubUrl = 'https://github.com/collinsefe/spring-boot-react-app.git'
+def gitCreds = 'prodigital-collinsefe'
+
+def environments = [
+    dev: [
+        name: 'Development',
+        branch: 'dev'
+    ],
+    test: [
+        name: 'Testing',
+        branch: 'test'
+    ],
+]
+
+environments.each { env, config ->
+    pipelineJob("MEDICAL-APP-${env.toUpperCase()}-Job") {
+        description('This Job is used to create the Node Server and is versioned. Changes should be made through the repo.')
+        keepDependencies(false)
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(githubUrl)
+                            credentials(gitCreds)
+                        }
+                        branch("*/${config.branch}")
+                    }
+                }
+            }
+        }
+        triggers {
+            githubPush()
+        }
+
+        properties {
+            disableConcurrentBuilds()
+        }
+    }
+}

73df8e40a3450e2083f3adf961d6acafcd9aed7f
Collins Orighose <collinsefe@yahoo.com>
2024-11-14 18:45:09 +0000

    change job names

--- seed-job-scripts/app_backend_seedjob.groovy
+++ seed-job-scripts/app_backend_seedjob.groovy
@@ -13,7 +13,7 @@ def environments = [
 ]
 
 environments.each { env, config ->
-    pipelineJob("MEDICAL-APP-${env.toUpperCase()}-Job") {
+    pipelineJob("MEDICAL-APP-BACKEND-${env.toUpperCase()}-Job") {
         description('This Job is used to create the Node Server and is versioned. Changes should be made through the repo.')
         keepDependencies(false)
 
